c5a75bf639a81edd8433de3d0a19dbcb718d2fb1
Aleksi Simell <aleksi.simell@eficode.com>
2016-08-22 10:42:37 +0300

    Continuing to implement ZAProxy support.

--- src/main/java/jenkinsci/plugins/influxdb/generators/ZAProxyPointGenerator.java
+++ src/main/java/jenkinsci/plugins/influxdb/generators/ZAProxyPointGenerator.java
@@ -1,17 +1,19 @@
 package jenkinsci.plugins.influxdb.generators;
 
+import jenkins.MasterToSlaveFileCallable;
+
 import org.influxdb.dto.Point;
 import org.jdom.JDOMException;
 import org.zaproxy.clientapi.core.Alert;
 import org.zaproxy.clientapi.core.AlertsFile;
-import org.zaproxy.clientapi.core.ClientApi;
 
 import hudson.FilePath;
 import hudson.model.Run;
 
-
 import java.io.File;
 import java.io.IOException;
+import java.lang.InterruptedException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -24,39 +26,50 @@ public class ZAProxyPointGenerator extends AbstractPointGenerator {
     private static final String INFORMATIONAL_ALERTS = "informational_alerts";
 
     private final Run<?, ?> build;
-    private final File zapFile;
-    private ClientApi clientapi;
+    private final FilePath zapFile;
 
     public ZAProxyPointGenerator(Run<?, ?> build, FilePath workspace) {
         this.build = build;
-        zapFile = new File(workspace + ZAP_REPORT_FILE);
+        zapFile = new FilePath(workspace, ZAP_REPORT_FILE);
     }
 
     public boolean hasReport() {
-        // TODO
-        return true;
-        //return (zapFile != null && zapFile.exists() && zapFile.canRead()); 
+        try {
+            return (zapFile != null && zapFile.exists());
+        } catch (IOException|InterrupedException e) {
+            // NOP
+        }
+        return false;
     }
 
     public Point[] generate() {
-        File file = new File("zap_report.html");
         try {
-            List<Alert> highAlerts = AlertsFile.getAlertsFromFile(file, "high");
-            List<Alert> mediumAlerts = AlertsFile.getAlertsFromFile(file, "medium");
-            List<Alert> lowAlerts = AlertsFile.getAlertsFromFile(file, "low");
-            List<Alert> informationalAlerts = AlertsFile.getAlertsFromFile(file, "ínformational");
+            List<int> ls = zapFile.act(new ZAPFileCallable());
             Point point = Point.measurement("zap_data")
                 .field(BUILD_NUMBER, build.getNumber())
                 .field(PROJECT_NAME, build.getParent().getName())
-                .field(HIGH_ALERTS, highAlerts.size())
-                .field(MEDIUM_ALERTS, mediumAlerts.size())
-                .field(LOW_ALERTS, lowAlerts.size())
-                .field(INFORMATIONAL_ALERTS, informationalAlerts.size())
+                .field(HIGH_ALERTS, ls.get(0))
+                .field(MEDIUM_ALERTS, ls.get(1))
+                .field(LOW_ALERTS, ls.get(2))
+                .field(INFORMATIONAL_ALERTS, ls.get(3))
                 .build();
             return new Point[] {point};
-        } catch (JDOMException|IOException e) {
-            return null;
+        } catch (IOException|InterruptedException e) {
+            // NOP
         }
+        return null;
     }
 
+    private static final class ZAPFileCallable extends MasterToSlaveFileCallable<List<int>> {
+        private static final long serialVersionUID = 1;
+
+        @Override
+        public List<int> invoke(File f, VirtualChannel channel) {
+            List<int> ls = new ArrayList<int>();
+            ls.add(AlertsFile.getAlertsFromFile(f, "high").size());
+            ls.add(AlertsFile.getAlertsFromFile(f, "medium").size());
+            ls.add(AlertsFile.getAlertsFromFile(f, "low").size());
+            ls.add(AlertsFile.getAlertsFromFile(f, "informational").size());
+            return ls;
+        }
 }
