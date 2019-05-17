4ecc22d549a4f5faf6f17aa6871a9355aa5a13f9
René Scheibe <rene.scheibe@gmail.com>
2019-05-17 15:57:41 +0200

    Don't rely on default encoding when reading SonarQube HTTP response
    This fixes the last issue found by SpotBugs.
    
    Using a higher level HTTP client instead of low level HttpURLConnection
    simplifies code, improves handling different scenarios (e.g. encoding)
    and provides more optimizations (e.g. connection & thread pools).
    
    NOTE: In commit 6d463b5 HttpURLConnection was introduced, replacing the
    former implementation using org.apache.httpcomponents. Unfortunately
    the commit comment does not contain any issue details.

--- src/main/java/jenkinsci/plugins/influxdb/generators/SonarQubePointGenerator.java
+++ src/main/java/jenkinsci/plugins/influxdb/generators/SonarQubePointGenerator.java
@@ -2,16 +2,17 @@ package jenkinsci.plugins.influxdb.generators;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.net.HttpURLConnection;
 import java.lang.InterruptedException;
-import java.util.Base64;
-import java.net.URL;
+
+import okhttp3.Credentials;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.apache.commons.lang3.StringUtils;
 import org.influxdb.dto.Point;
 
@@ -38,6 +39,8 @@ public class SonarQubePointGenerator extends AbstractPointGenerator {
 	private static final String SONAR_ISSUES_BASE_URL = "/api/issues/search?ps=500&projectKeys=";
 
 	private static final String SONAR_METRICS_BASE_URL = "/api/measures/component?metricKeys=ncloc,complexity,violations&componentKey=";
+
+	private static final OkHttpClient httpClient = new OkHttpClient();
 
 	private String sonarIssuesUrl;
 	private String sonarMetricsUrl;
@@ -76,7 +79,7 @@ public class SonarQubePointGenerator extends AbstractPointGenerator {
 			String url = "";
 			try {
 				url = build.getEnvironment(listener).get("SONAR_HOST_URL");
-			} catch (InterruptedException|IOException e) {
+			} catch (InterruptedException | IOException e) {
 				// handle
 			}
 			String sonarServer;
@@ -116,45 +119,29 @@ public class SonarQubePointGenerator extends AbstractPointGenerator {
 		return new Point[] { point };
 	}
 
-	private String getResult(String request) throws IOException {
-		StringBuilder result = new StringBuilder();
-		
+	private String getResult(String url) throws IOException {
+		Request.Builder requestBuilder = new Request.Builder()
+				.get()
+				.url(url)
+				.header("Accept", "application/json");
+
 		try {
-			String auth = "";
-			try {
-				String token = build.getEnvironment(listener).get("SONAR_AUTH_TOKEN");
-				if (token != null) {
-					token = token + ":";
-					String encoding = Base64.getEncoder().encodeToString(token.getBytes(StandardCharsets.UTF_8));
-					auth = "Basic " + encoding;
-				}
-			} catch (InterruptedException|IOException e) {
-				// handle
+			String token = build.getEnvironment(listener).get("SONAR_AUTH_TOKEN");
+			if (token != null) {
+				String credential = Credentials.basic(token, "", StandardCharsets.UTF_8);
+				requestBuilder.header("Authorization", credential);
 			}
-			URL url = new URL(request);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			if (!auth.isEmpty())
-				conn.setRequestProperty("Authorization", auth);
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Accept", "application/json");
-
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode() + " from URL : " + conn.getURL());
-			}
-
-			try (BufferedReader rd = new BufferedReader(new InputStreamReader((conn.getInputStream())))) {
-				String line;
-				while ((line = rd.readLine()) != null) {
-					result.append(line);
-				}
-			}
-
-			conn.disconnect();
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (InterruptedException | IOException e) {
+			// handle
 		}
 
-		return result.toString();
+		try (Response response = httpClient.newCall(requestBuilder.build()).execute()) {
+			if (response.code() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.code() + " from URL : " + url);
+			}
+
+			return response.body().string();
+		}
 	}
 
 	private String getSonarProjectURLFromBuildLogs(Run<?, ?> build) throws IOException {
